a8437500f2efb6e261bab71b824fd7d804e11336
Lehr <mark.lehr@rcc.edu>
2022-03-22 12:11:10 -0700

    Signed-off-by: Lehr <mark.lehr@rcc.edu>

---
+++ Class/Grade_Dependent_If/main.cpp
@@ -0,0 +1,54 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark Lehr
+ * Created on March 8th, 2022, 11:49 AM
+ * Purpose:  C++ Template
+ */
+
+//System Libraries
+#include <iostream>  //Input/output Library
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Physics/Chemistry/Math/Conversion Higher Dimension Only
+
+//Function Prototypes
+
+//Program Execution Begins Here!!!
+int main(int argc, char** argv) {
+    //Set the Random Number Seed
+    srand(static_cast<unsigned int>(time(0)));
+    
+    //Declare Variables
+    unsigned char score,//Score for a homework [50,100]
+                  grade;//Typical Grade
+    
+    //Initial Variables
+    score=rand()%51+50;//[50,100]
+    
+    //Map the Inputs to the Outputs
+    if(score>=90){
+        grade='A';
+    }else if(score>=80){
+        grade='B';
+    }else if(score>=70){
+        grade='C';
+    }else if(score>=60){
+        grade='D';
+    }else{
+        grade='F';
+    }
+          
+    //Display the Inputs and Outputs
+    cout<<"Your score of "<<static_cast<int>(score)
+        <<" = "<<grade<<endl;
+
+    //Exit the code
+    return 0;
+}
+
