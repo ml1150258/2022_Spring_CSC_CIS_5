660aad7a5448287dfa6ae9fb1b45e82cabd7c04a
ml1150258 <mark.lehr@rcc.edu>
2022-04-20 09:50:32 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Projects/GameOfCraps/Craps_V4/nbproject/Makefile-variables.mk
+++ Projects/GameOfCraps/Craps_V4/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=MinGW-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
-CND_ARTIFACT_NAME_Debug=craps_v2
-CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/craps_v2
+CND_ARTIFACT_NAME_Debug=craps_v4
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/craps_v4
 CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
-CND_PACKAGE_NAME_Debug=crapsv2.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/crapsv2.tar
+CND_PACKAGE_NAME_Debug=crapsv4.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/crapsv4.tar
 # Release configuration
 CND_PLATFORM_Release=MinGW-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
-CND_ARTIFACT_NAME_Release=craps_v2
-CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/craps_v2
+CND_ARTIFACT_NAME_Release=craps_v4
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/craps_v4
 CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
-CND_PACKAGE_NAME_Release=crapsv2.tar
-CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/crapsv2.tar
+CND_PACKAGE_NAME_Release=crapsv4.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/crapsv4.tar
 #
 # include compiler specific variables
 #
